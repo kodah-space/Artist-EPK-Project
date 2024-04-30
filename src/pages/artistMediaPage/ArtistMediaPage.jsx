d4d4b078acc63deb94dd15257e78e4627a6f5ea0
Noelia <sharimanoe@gmail.com>
2024-04-30 16:05:44 +0200

    first commit: router and new pages

---
+++ src/pages/artistMediaPage/ArtistMediaPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ArtistMediaPage() {
+  return (
+    <div className="artistMediaPage-container">
+      <h1>Artist Media Page</h1>
+    </div>
+  );
+}
+
+export default ArtistMediaPage;
---
+++ src/pages/contactPage/ContactPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ContactPage() {
+  return (
+    <div className="contactPage-container">
+      <h1>This is the Contact Page</h1>
+    </div>
+  );
+}
+
+export default ContactPage;
