8bb3329dfd0925f712ad8e70d177294640f7ba6a
Mira Witte <mira.witte@gmx.de>
2024-05-02 17:52:56 +0200

    Tailwind setup and Navbar

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <a
+        href="https://github.com/kodah-space/Artist-EPK-Project"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <p className="footer-text">github repo</p>
+      </a>
+    </footer>
+  );
+}
