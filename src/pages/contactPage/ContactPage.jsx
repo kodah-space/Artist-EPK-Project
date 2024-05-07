0a87e9d885ea452a339b2a9aa9140041bab68630
Mira Witte <mira.witte@gmx.de>
2024-05-07 22:18:35 +0200

    styling fixes

--- src/pages/contactPage/ContactPage.jsx
+++ src/pages/contactPage/ContactPage.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 function ContactPage() {
   return (
-    <div className="contactPage-container">
-      <h1>This is the Contact Page</h1>
+    <div className="contactPage-container font-[Linux-Libertine] text-xl p-4 mb-4 md:px-20">
+      <h3 className="pb-5">Get in Touch!</h3>
+      <p>mink</p>
+      <p>Madergasse 13</p>
+      <p>10000 Ottershausen</p>
+      <p>phone: +49-30 3727337</p>
+      <p>hello@mink.com</p>
     </div>
   );
 }
